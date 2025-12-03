3241e7138c4824d83ad6e02d78262a4195527098
david martinez <daviddarkghost@gmail.com>
2025-12-03 11:52:20 +0100

    day 3 done, im a god, i like this algorith

---
+++ day3/2/main.py
@@ -0,0 +1,35 @@
+fich = open('entrega', 'r')
+
+
+def max(num,pos,final):
+    max=num[pos]
+    newpos=pos
+    for i in range (pos+1,final):
+        if(num[i]>max):
+            max=num[i]
+            newpos=i
+    return max, newpos
+
+res=""
+vec=[]
+for num in fich:
+    print(num)
+    dig=0
+    pos=0
+    i=11
+    limit=i+1
+    while(dig<limit):
+        final=len(num)-i-1
+        newdig, newpos=max(num,pos,final)
+        pos=newpos+1
+        i-=1
+        res= res + newdig
+        dig+=1
+    print(res)
+    vec.append(res)
+    res=""
+print(vec)
+sum=0
+for i in range(len(vec)):
+    sum+=int(vec[i])
+print(f"Res: {sum}")
